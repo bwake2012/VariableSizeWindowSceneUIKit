4909c64fe3d0fee8239e650fdbe0b442de945f30
Bob Wakefield <bob@cockleburr.net>
2024-05-05 22:52:57 -0500

    Initial Commit

---
+++ VariableSizeWindowSceneUIKit/ViewController.swift
@@ -0,0 +1,23 @@
+// ViewController.swift
+//
+// Created by Bob Wakefield on 5/5/24.
+// for VariableSizeWindowSceneUIKit
+//
+// Using Swift 5.0
+// Running on macOS 14.4
+//
+// 
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
